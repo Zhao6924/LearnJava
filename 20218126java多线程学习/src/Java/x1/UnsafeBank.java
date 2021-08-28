571cb057b5cc9844bd338c99c7f3e1cba0283710
leixian zhao <905047438@qq.com>
2021-08-28 18:13:56 +0800

    学习synchronize锁

--- 20218126java多线程学习/src/Java/x1/UnsafeBank.java
+++ 20218126java多线程学习/src/Java/x1/UnsafeBank.java
@@ -1,7 +1,77 @@
 package Java.x1;
-
 public class UnsafeBank {
+
     public static void main(String[] args) {
-        System.out.println("unsafe");
+        //账户
+        Account account=new Account(100,"结婚基金");
+        Drawing you=new Drawing(account,100,"你");
+        Drawing girlf=new Drawing(account,50,"女朋友");
+
+        you.start();
+        girlf.start();
+
     }
+}
+class Account{
+    int monney;
+    String name;
+    public Account(int monney,String name)
+    {
+        this.monney=monney;
+        this.name=name;
+    }
+};
+class Drawing extends Thread{
+    Account account;
+    //收了多少钱
+    int drawMoney;
+    //现在手里有多少钱
+    int nowMoney;
+    public  Drawing(Account count,int drawingMoney,String name)
+    {
+        super(name);
+        this.account=count;
+        this.drawMoney=drawingMoney;
+       // this.nowMoney=nowMoney;
+    }
+
+/*   @Override
+    public void run(){
+        if (account.monney-this.drawMoney<0)
+        {
+            System.out.println(Thread.currentThread().getName()+"取不了，钱不够");
+            return ;
+        }
+       try {
+           Thread.sleep(1000);
+       } catch (InterruptedException e) {
+           e.printStackTrace();
+       }
+       account.monney=account.monney-drawMoney;
+        nowMoney=nowMoney+drawMoney;
+        //手上剩下的钱
+       System.out.println(Thread.currentThread().getName()+"余额"+this.nowMoney);
+       //账户的余度
+       System.out.println(Thread.currentThread().getName()+"账户额度"+this.account.monney);
+   }*/
+@Override
+public synchronized void run() {
+    synchronized (account) {
+        if (account.monney - this.drawMoney < 0) {
+            System.out.println(Thread.currentThread().getName() + "取不了，钱不够");
+            return;
+        }
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        account.monney = account.monney - drawMoney;
+        nowMoney = nowMoney + drawMoney;
+        //手上剩下的钱
+        System.out.println(Thread.currentThread().getName() + "余额" + this.nowMoney);
+        //账户的余度
+        System.out.println(Thread.currentThread().getName() + "账户额度" + this.account.monney);
+    }
+}
 }
--- 20218126java多线程学习/src/Java/x1/UnsafeBuyTicket.java
+++ 20218126java多线程学习/src/Java/x1/UnsafeBuyTicket.java
@@ -2,9 +2,11 @@ package Java.x1;
 //不安全的买票
 public class UnsafeBuyTicket {
     public static void main(String[] args) {
-        new Thread(new BuyTicket(),"我").start();
-        new Thread(new BuyTicket(),"黄牛").start();
-        new Thread(new BuyTicket(),"其他人").start();
+        BuyTicket ticketStation =new BuyTicket();
+
+        new Thread(ticketStation,"我").start();
+        new Thread(ticketStation,"黄牛").start();
+        new Thread(ticketStation,"其他人").start();
     }
 
 }
@@ -13,13 +15,20 @@ class BuyTicket implements Runnable{
     private  int ticket=10;
     @Override
     public void run(){
-        for (int i=0;i<10;i++)
-            this.buy();
+        for (int i=0;i<10;i++) {
+            try {
+                this.buy();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
-    private void buy(){
+    //锁的是this
+    private synchronized void buy() throws InterruptedException {
         if (ticket<=0)
             return ;
-        System.out.println(Thread.currentThread().getName()+"买完还剩"+this.ticket);
+        Thread.sleep(100);
+        System.out.println(Thread.currentThread().getName()+"买完还剩"+ --this.ticket);
     }
 
 }
